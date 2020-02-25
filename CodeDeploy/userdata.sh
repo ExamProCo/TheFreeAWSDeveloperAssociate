e30c622d703425d9e3447fcf5e17523739a316d6
Andrew Brown <andrew@monsterboxpro.com>
2020-02-24 21:30:38 -0500

    codedeploy

---
+++ CodeDeploy/userdata.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+su ec2-user
+sudo yum install httpd -y
+sudo chown ec2-user:ec2-user -R /var/www
+cat > /var/www/html/index.html <<EOL
+<html>
+  <head>
+    <title>My WebApp</title>
+  </head>
+  <body>
+     <h1>My WebApp</h1>
+  </body>
+</html>
+EOL
+sudo systemctl start httpd
+sudo systemctl enable httpd
