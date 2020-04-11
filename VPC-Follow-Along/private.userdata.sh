3f5b312c9003f1b7520b5559b0fbfb15dfeafe04
aroup <aroupdhruba@gmail.com>
2020-04-11 18:12:07 +0600

    Edit private user data

--- VPC-Follow-Along/private.userdata.sh
+++ VPC-Follow-Along/private.userdata.sh
@@ -1,4 +1,4 @@
 su ec2-user
-echo -e "randompassword" | passwd ec2-user
+echo -e "randompassword\nrandompassword" | passwd ec2-user
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
