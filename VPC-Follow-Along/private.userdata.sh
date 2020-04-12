c2388349a082478b17c42585f2b3cded2a593798
aroup <aroupdhruba@gmail.com>
2020-04-13 01:03:43 +0600

    Add bash script

--- VPC-Follow-Along/private.userdata.sh
+++ VPC-Follow-Along/private.userdata.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 su ec2-user
 echo -e "randompassword\nrandompassword" | passwd ec2-user
 sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
